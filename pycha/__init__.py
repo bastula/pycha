e078a01dd92458b07312216e6b4c585ac91986ed
Lorenzo Gil S?nchez <lorenzo.gil.sanchez@gmail.com>
2009-09-26 19:26:23 +0200

    Bump version number

--- pycha/__init__.py
+++ pycha/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyCha.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "0.5.2"
+version = "0.5.3dev"
