c73655094e68725a75a473a3a8ee18c85584e197
Lorenzo Gil S?nchez <lorenzo.gil.sanchez@gmail.com>
2010-03-28 10:10:45 +0200

    Remove unused files

--- setup.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pkg=pycha
-. simple-setup.bash
-
-version=$( python -c "import sys; print '.'.join(map(str,sys.version_info[:2]))" )
-install lib/python$version/site-packages/ pycha
